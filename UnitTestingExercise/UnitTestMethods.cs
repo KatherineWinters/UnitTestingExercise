a95b76e0df670765e8e2cc6248ec9b83dcb6d469
Katherine Winters <goble.katherine@gmail.com>
2021-12-29 20:40:37 -0600

    initail commit

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,44 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public object Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
-
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public object Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
         // Create a Multiply method that passes 2 integers
-
+        public object Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         // Create a Divide method that passes 2 integers
+        public object Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
+        public bool IsOdd(int num)
+        {
+            if (num % 3 == 0)
+                return true;
+            else
+                return false;
+        }
 
-
+        public bool IsGreaterThanA(int a, int b)
+        {
+            if (a < b)
+                return true;
+            else
+                return false;
+        }
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+
     }
 }
---
+++ UnitTestingExercise/UnitTestMethodsBase.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTestingExercise
+{
+    public class UnitTestMethodsBase
+    {
+
+        public bool IsGreaterThanNum1(int 3, int 4)
+        {
+            if (3 < 4)
+                return true;
+            else
+                return false;
+        }
+    }
+}
